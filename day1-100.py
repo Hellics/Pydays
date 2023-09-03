7dcc8247dae883af687b66a317bea4b47ef3e2b2
Emiliano Lecce <hellmiliano@gmail.com>
2023-09-03 20:34:18 +0200

    add day 1 fiel

---
+++ day1-100.py
@@ -0,0 +1,13 @@
+#bandname generator
+
+#welcome message
+print("Welcome to the bandname generator."
+
+#ask user city name
+city=input("What's name of the city you grew up in?\n")
+
+#ask user's pet name
+pet=input("What'syour pet name?\n")
+
+#bandname output
+print("Your bandname could be "+ city +" "+pet)
