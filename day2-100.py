32c259ce78affc732a6fab99ec47ff55ee2d1fd4
Emiliano <hellmiliano@gmail.com>
2023-09-04 23:25:38 +0200

    Prompt

--- day2-100.py
+++ day2-100.py
@@ -1,3 +1,12 @@
 #second day project
 #adding code soon
 
+#Welcome to the tip calculator
+
+#What was the total bill? $124.54
+
+#How many people to split the bill? 5
+
+#What percentage tip would you like to give? 10, 12, or 15? 12
+
+#Each person should pay: $27.9
