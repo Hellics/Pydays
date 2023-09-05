29fbd5d30dcac3dfe6c5dc410c7af1c3158f10e5
Emiliano <hellmiliano@gmail.com>
2023-09-05 18:57:03 +0200

    some line

--- day2-100.py
+++ day2-100.py
@@ -2,11 +2,14 @@
 #adding code soon
 
 #Welcome to the tip calculator
-print("Wecome to the tip calculator!")
+print("Welcome to the tip calculator!\n")
+
 #What was the total bill? $124.54
+int(input("What was the total bill?\n"))
+
 
 #How many people to split the bill? 5
-
+int(input("How many people to split the bill?\n"))
 #What percentage tip would you like to give? 10, 12, or 15? 12
 
 #Each person should pay: $27.9
