275b5d425024c763b759743940e0b8eb65dd5d01
Emiliano <hellmiliano@gmail.com>
2023-09-06 22:27:26 +0200

    near ending

--- day2-100.py
+++ day2-100.py
@@ -1,18 +1,21 @@
 #second day project
-#adding code soon
+
+#Tip calculator
 
 #Welcome to the tip calculator
-print("Welcome to the tip calculator!\n")
+print("Welcome to the tip calculator!")
 
-#What was the total bill? $124.54
-bill=float(input("What was the total bill?\n"))
-print(f"Total bill equal to {bill}")
+#collecting data from input
+#What was the total bill? $124.56
+bill=float(input("What was the total bill?"))
 
 #How many people to split the bill? 5
-person=int(input("How many people to split the bill?\n"))
+people=int(input("How many people to split the bill?"))
 
 #What percentage tip would you like to give? 10, 12, or 15? 12
-perce=int(input("What percentage tip woulf you like to give? 10, 12  or 15\n"))
-
+tip=int(input("What percentage tip would you like to give? 10, 12  or 15"))
+tip_perc=tip/100*bill+bill
+print(tip_perc)
+print(f"Each person should pay: $ ", tip_perc/people)
 #Each person should pay: $27.9
 
