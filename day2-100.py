18c9f928722f9e731b0c08d7dddc7eec4891a000
Emiliano Lecce <hellmiliano@gmail.com>
2023-09-09 15:30:32 +0200

    Final

--- day2-100.py
+++ day2-100.py
@@ -7,15 +7,19 @@ print("Welcome to the tip calculator!")
 
 #collecting data from input
 #What was the total bill? $124.56
-bill=float(input("What was the total bill?"))
+bill=float(input("What was the total bill? "))
 
 #How many people to split the bill? 5
-people=int(input("How many people to split the bill?"))
+people=int(input("How many people to split the bill? "))
 
 #What percentage tip would you like to give? 10, 12, or 15? 12
-tip=int(input("What percentage tip would you like to give? 10, 12  or 15"))
-tip_perc=tip/100*bill+bill
+tip=int(input("What percentage tip would you like to give? 10, 12  or 15 "))
+tip_perc = tip/100
+tot_tip = tip_perc*bill
+tot_bill = tot_tip+bill
+bill_per_person = tot_bill/people
+final_ammount = round(bill_per_person,2)
 
 #Each person should pay: 
-print(f"Each person should pay: $ "),print(round(tip_perc/people,2))
+print(f"Each person should pay: $ {final_ammount}")
 
